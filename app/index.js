e28914252a2a99c56297ba4319f50b1671faa32c
Pedro Pena <ppena.it@gmail.com>
2017-12-20 13:58:05 -0500

    1stcommit

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
+
+EStyleSheet.build({
+  $primaryblue: '#4f6d7a',
+});
+
+export default () => <Home />;
